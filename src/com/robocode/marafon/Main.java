8d3cef53d8d6ba4801df37b9cc620be04ec36145
AlexanderIvanov <alexivanov2890@mail.ru>
2022-12-19 15:31:39 +0300

    task_3, task_4, task_6, task_7

--- src/com/robocode/marafon/Main.java
+++ src/com/robocode/marafon/Main.java
@@ -29,14 +29,62 @@ public class Main {
 //        System.out.print(Arrays.toString(mass));
 
         // ------task-3-------//
-        Scanner scanner = new Scanner(System.in);
-        int[] mass = new int[10];
-        for (int i = 0; i < mass.length; i++) {
-            System.out.print("Enter number ");
-            int y = scanner.nextInt();
-            mass[i] = y;
-        }
-        System.out.print(Arrays.toString(mass));
+//        Scanner scanner = new Scanner(System.in);
+//        int[] mass = new int[10];
+//        for (int i = 0; i < mass.length; i++) {
+//            System.out.print("Enter number ");
+//            int y = scanner.nextInt();
+//            mass[i] = y;
+//        }
+//        for(int i=0; i<mass.length-1; i++) {
+//            for (int j=i+1; j<mass.length; j++) {
+//                if(mass[i] == mass[j]) {
+//                    System.out.println(mass[j]);
+//                }
+//            }
+//        }
+
+        // ------task-5-------//
+//        int[] mass = new int[10];
+//        for (int i = 0; i < mass.length; i++) {
+//            int a = (int) (Math.random()*(100+1)) - 50;
+//            mass[i] = a;
+//        }
+//        System.out.print(Arrays.toString(mass));
+
+        // ------task-6-------//
+//        int[] mass = new int[10];
+//        for (int i = 0; i < mass.length; i++) {
+//            int a = (int) (Math.random()*(40+1)) - 20;
+//            mass[i] = a;
+//        }
+//        for(int i = 0; i<mass.length-1; i++) {
+//            for (int j = i+1; j<mass.length; j++) {
+//                if(mass[i] > mass[j]) {
+//                    int temp = mass[i];
+//                    mass[i] = mass[j];
+//                    mass[j] = temp;
+//                }
+//            }
+//        }
+//        System.out.println(Arrays.toString(mass));
+
+        // ------task-7-------//
+//        int[] mass = new int[10];
+//        for (int i = 0; i < mass.length; i++) {
+//            int a = (int) (Math.random()*(40+1)) - 20;
+//            mass[i] = a;
+//        }
+//        for (int i = mass.length - 1; i > 0; i--) {
+//            for (int j = 0; j < i; j++) {
+//                if (mass[j] < mass[j+1]) {
+//                    int tmp = mass[j];
+//                    mass[j] = mass[j+1];
+//                    mass[j+1] = tmp;
+//                }
+//            }
+//        }
+//        System.out.println(Arrays.toString(mass));
 
     }
 
